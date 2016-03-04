007b7f6114b7efcc4ed943efe5cb916ceab9a2bc
FRAJ <458734857@qq.com>
2016-03-04 22:23:36 +0800

    创建collectionviewCell类

---
+++ FJInfiniteScroll_C/FJInfiniteScroll_C/FJImageCell.h
@@ -0,0 +1,14 @@
+//
+//  FJImageCell.h
+//  FJInfiniteScroll_C
+//
+//  Created by Francis on 16/3/4.
+//  Copyright © 2016年 FRAJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FJImageCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
---
+++ FJInfiniteScroll_C/FJInfiniteScroll_C/FJImageCell.m
@@ -0,0 +1,13 @@
+//
+//  FJImageCell.m
+//  FJInfiniteScroll_C
+//
+//  Created by Francis on 16/3/4.
+//  Copyright © 2016年 FRAJ. All rights reserved.
+//
+
+#import "FJImageCell.h"
+
+@implementation FJImageCell
+
+@end
