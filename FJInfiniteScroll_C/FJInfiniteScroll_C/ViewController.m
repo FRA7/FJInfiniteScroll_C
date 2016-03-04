93ac3e14872c9d041841135d10e60f572817c4c2
FRAJ <458734857@qq.com>
2016-03-04 22:54:05 +0800

    创建collectionView

--- FJInfiniteScroll_C/FJInfiniteScroll_C/ViewController.m
+++ FJInfiniteScroll_C/FJInfiniteScroll_C/ViewController.m
@@ -7,21 +7,40 @@
 //
 
 #import "ViewController.h"
+#import "FJImageCell.h"
 
-@interface ViewController ()
+@interface ViewController ()<UICollectionViewDataSource,UICollectionViewDelegate>
 
 @end
 
 @implementation ViewController
 
+static NSInteger FJItemCount = 200;
+static NSString *FJImageCellID = @"FJImageCellID";
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    //layout
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    layout.minimumLineSpacing = 0;
+    
+    //UICollectionView
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 200) collectionViewLayout:layout];
+    collectionView.dataSource = self;
+    collectionView.delegate = self;
+    collectionView.pagingEnabled = YES;
+    collectionView.showsHorizontalScrollIndicator = NO;
+    collectionView.showsVerticalScrollIndicator = NO;
+    [self.view addSubview:collectionView];
+    
+    layout.itemSize = collectionView.frame.size;
+    
+    //注册cell
+    [collectionView registerNib:[UINib nibWithNibName:NSStringFromClass([FJImageCell class]) bundle:nil] forCellWithReuseIdentifier:FJImageCellID];
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
